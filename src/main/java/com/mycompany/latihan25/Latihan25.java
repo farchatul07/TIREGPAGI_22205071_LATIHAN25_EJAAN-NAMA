c7f5baf72841641d3eab5423521fa9dd5884706d
farchatul07 <145920038+farchatul07@users.noreply.github.com>
2023-10-26 19:16:18 +0700

    Add files via upload

---
+++ src/main/java/com/mycompany/latihan25/Latihan25.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.latihan25;
+import java.util.Scanner;
+        
+/**
+ *
+ * @author user
+ */
+public class Latihan25 {
+
+    public static void main(String[] args) {
+        //TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        
+        boolean ulangi = true;
+        while(ulangi){
+            System.out.print("Masukkan nama depan anda untuk dieja : ");
+            String nama = input.next();
+            
+            System.out.println("Ejaan untuk '"+nama+"' adalah ");
+            
+            int panjangNama = nama.length();
+            for(int i = 0; i<panjangNama; i++){
+                char huruf = nama.charAt(i);
+                System.out.println("Huruf ke-"+(i+1)+" : "+huruf);
+            }
+            
+            System.out.print("Ulangi Aktifitas? (ya/tidak)");
+            String ulang = input.next();
+            if(ulang == "ya"){
+                System.out.println("Program akan diulangi");
+            }else if(ulang == "tidak"){
+                System.out.println("Program berhenti terimakasih");
+                ulangi = false; 
+        }
+        }
+    }
+    
+}
